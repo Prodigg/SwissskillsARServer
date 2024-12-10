9e19310de108848d3e2edaf2f3c11b86c45f7235
Prodigg <raphael@familie-sauer.ch>
2024-12-10 21:22:52 +0100

    first succsessfull communication

---
+++ Tools/minimalTCPServer.py
@@ -0,0 +1,29 @@
+import asyncio
+import websockets
+
+# Set of connected clients
+connected_clients = set()
+
+# Function to handle each client connection
+async def handle_client(websocket, path):
+    # Add the new client to the set of connected clients
+    print("client")
+    connected_clients.add(websocket)
+    try:
+        # Listen for messages from the client
+        async for message in websocket:
+            print(message)
+    except websockets.exceptions.ConnectionClosed:
+        pass
+    finally:
+        # Remove the client from the set of connected clients
+        connected_clients.remove(websocket)
+
+# Main function to start the WebSocket server
+async def main():
+    server = await websockets.serve(handle_client, '192.168.3.223', 1880)
+    await server.wait_closed()
+
+# Run the server
+if __name__ == "__main__":
+    asyncio.run(main())
